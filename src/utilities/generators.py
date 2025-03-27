eaea1d7c86d5e0a19299cab268f8a644328f1abb
Akiaksei Yasinetski <you@example.com>
2025-03-27 13:13:28 +0300

    Add similar matrix computing, when correlation matrix is not positive semidefined.

--- src/utilities/generators.py
+++ src/utilities/generators.py
@@ -30,9 +30,18 @@ class CorrelatedNormalGenerator(NormalGenerator):
         super(CorrelatedNormalGenerator, self).__init__(mean, variance, 
                                                         dimension)
         
-        if any(np.linalg.eig(correlation_matrix)[0] < 0):
-            raise Exception("Invalid correlation matrix, " +
-                "it should be positive semi-definite") 
+        eigen_values = np.linalg.eig(correlation_matrix)[0]
+        
+        if any(eigen_values < 0):
+            correlation_matrix = self._compute_similar_matrix(
+                correlation_matrix
+            )
+
+            print(
+                f"Invalid correlation matrix, " + 
+                f"it should be positive semi-definite. " + 
+                f"Using similar correlation matrix: {correlation_matrix}"
+            ) 
 
         self.correlation_matrix = np.linalg.cholesky(
             np.array(correlation_matrix)
@@ -51,5 +60,17 @@ class CorrelatedNormalGenerator(NormalGenerator):
                 for i in range(self.dimension)
             ]
 
-            correlated_observation = self.correlation_matrix.dot(observation)
+            correlated_observation = self.correlation_matrix @ observation
+
             yield correlated_observation
+
+    def _compute_similar_matrix(self, matrix: np.array) -> np.array:
+        eigen_values, eigen_vectors = np.linalg.eig(matrix)
+        eigen_values[eigen_values < 0] = 1e-3
+
+        inversed_eigen_vectors = np.linalg.inv(eigen_vectors)
+        eigen_diagonal = np.diag(eigen_values)
+        
+        similar_matrix = eigen_vectors @ eigen_diagonal @ inversed_eigen_vectors
+        
+        return similar_matrix
